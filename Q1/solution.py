336a4027db88bc33c619853023eaf0b193fe58b5
gurk <gurkmandur@gmail.com>
2019-12-02 14:28:20 -0500

    solution to q1

---
+++ Q1/solution.py
@@ -0,0 +1,17 @@
+import os
+
+
+def fuel_calculator(mass):
+    return (mass // 3) - 2
+
+
+filepath = f'{os.getcwd()}\Q1\input.txt'
+f = open(filepath, 'r')
+# calcuate total fuel cost
+total = 0
+for line in f:
+    mass = int(line)
+    total += fuel_calculator(mass)
+f.close()
+
+print(total)
